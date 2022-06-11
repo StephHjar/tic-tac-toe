b25c580b8f635d77ff8988dfef1a0faafb89d7d0
Steph Hjartarson <stephanie.hja@gmail.com>
2022-06-11 12:23:29 +0000

    Set up access to high scores Google Sheet via APIs

---
+++ run.py
@@ -0,0 +1,20 @@
+"""Import external libraries"""
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('tic_tac_toe_scores')
+
+scores = SHEET.worksheet('high_scores')
+
+data = scores.get_all_values()
+
+print(data)
