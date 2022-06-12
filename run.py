ec802eabe641c03ec058d7c8692fe607e7acbf1a
Steph Hjartarson <stephanie.hja@gmail.com>
2022-06-12 11:44:31 +0000

    Add validation function to make sure username is a valid input

--- run.py
+++ run.py
@@ -28,9 +28,54 @@ print(f"""{Fore.CYAN}{Style.BRIGHT}                                 WELCOME TO
     ██    ██  ██████        ██    ██   ██  ██████        ██     ██████  ███████
    """)
 
-username = input("What is your username?\n")
-print("\nWelcome, " + str(username) + """! Let's get started!\n
-What would you like to do first?\n
-1 - Read Instructions
-2 - Start a New Game
-3 - View High Scores""")
+
+def get_username():
+    """Ask the player to provide a username,
+    and check whether the input is valid"""
+    while True:
+        username = input("Please enter a username of 8 letters or less:\n")
+        if validate_username(username):
+            print("\nWelcome, " + str(username) + "! Let's get started!")
+            display_welcome_menu()
+            break
+
+    return username
+
+
+def validate_username(username):
+    """Check the chosen username to make sure it meets all criteria:
+    8 characters or less, using only letters."""
+    try:
+        if len(username) > 8:
+            raise ValueError(
+                f"""This username is {len(username)} characters long.
+                Please use 8 characters or less."""
+            )
+        if username.isalpha() is False:
+            raise ValueError(
+                """This username contains characters that are not letters in
+                the English alphabet."""
+            )
+    except ValueError as e:
+        print(f"Invalid username: {e}\n")
+        return False
+
+    return True
+
+
+def display_welcome_menu():
+    """Display the menu options to the player after they enter a username.
+    They can choose to read the instructions, start a new game, or view the
+    high score board."""
+    print("""\nWhat would you like to do first?\n
+    1 - Read Instructions
+    2 - Start a New Game
+    3 - View High Scores""")
+
+
+get_username()
+
+
+menu_choice = input("Enter a number:\n")
+if str(menu_choice) == "1":
+    print("here are the instructions")
