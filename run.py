47f53fde4769095d49077759ecb8ee7bc4c8949a
Steph Hjartarson <stephanie.hja@gmail.com>
2022-06-14 10:36:07 +0000

    Create a list of winning cell combinations

--- run.py
+++ run.py
@@ -27,6 +27,10 @@ high_score_data = high_scores.get_all_values()
 
 scores = {"computer": 0, "player": 0}
 guesses = []
+possible_wins = [
+                [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8],
+                [3, 6, 9], [1, 5, 9], [3, 5, 7]
+                ]
 
 
 class Board():
